49d4725f348ee16a1248936c90c1d388a3e557f0
Africa K <afric@Golden-Roses.local>
2022-03-26 13:48:08 -0400

    added randomizer with Z and psuedo code

--- js/strechBreak.js
+++ js/strechBreak.js
@@ -5,19 +5,27 @@ const handsup = document.querySelector('#handsup')
 
 
 document.querySelector('#eyesnext').addEventListener('click', eyesnext)
-document.querySelector('#necknext').addEventListener('click', necknext)
-document.querySelector('#shouldersnext').addEventListener('click', shouldersnext)
-document.querySelector('#handsnext').addEventListener('click', handsnext)
+//document.querySelector('#necknext').addEventListener('click', necknext)
+//document.querySelector('#shouldersnext').addEventListener('click', shouldersnext)
+//document.querySelector('#handsnext').addEventListener('click', handsnext)
 
 
 function eyesnext(){
+    let filePath = 'img/eyes/'
+    let eyeImages = ['adream.png','background1.png','blurfloat.gif', 'couchlife.png','driftfloat.gif']
+//run random function so it spits out random number
+
+//set src for eyes up to whatever image in our array has the index of the random number that was spit out
+eyesup.src = filePath + eyeImages[getRandomInt(5)]
+//google how to set up src using javascript
+//add function so that it doesnt toggle
     neckup.classList.add('hidden')
     shouldersup.classList.add('hidden')
     handsup.classList.add('hidden')
     eyesup.classList.toggle('hidden')
 }
 
-
+/*
 function necknext(){
     eyesup.classList.add('hidden')
     shouldersup.classList.add('hidden')
@@ -41,20 +49,43 @@ function handsnext(){
     handsup.classList.toggle('hidden')
 }
 
+*/
+
 //create a function that creates random pictures to be shown upon a click event
-randomImageEyes()
+
 
 //Create a collection of images for each click event (applies to each click event)
+// create an aray with 5 image strings in it
+//finds the file path and calls it
+
+
+
+//console.log(filePath + eyeImages[0])
+//create variable that holds image paths
 
 //where are you going to store images? [in a folder, in its own folder? what would  make this easiest?]
-//pull images from where they aer located and put them in an array
+//pull images from where they are located and put them in an array
+
+//how do you want it selected randomly
+//want a randomized number spit out from 0-4
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+
+
+
+
 
 
 //eye click event (5 gifs) [how are you going to do this?]
+
 //one image randomly displays per each click
 //whatever picture is already up, needs to go to hidden
 //select one photo out of 5 in eyeclick array
-//how do you want it selected?
+
+
 //want one randomly selected
 //need function that randomizes a number 0-4 that corresponds with image in array
 //once number is selected in array
