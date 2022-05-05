4e28bacde342c4f6faeab88b0da807277d84bbaa
Lauri Jalonen <ljalonen@seatosummit.com.au>
2022-05-05 18:16:19 +0800

    formatted files, fixed CSS typo

--- js/strechBreak.js
+++ js/strechBreak.js
@@ -1,28 +1,32 @@
-const eyesup = document.querySelector('#eyesup')
-const neckup = document.querySelector('#neckup')
-const shouldersup = document.querySelector('#shouldersup')
-const handsup = document.querySelector('#handsup')
+const eyesup = document.querySelector("#eyesup");
+const neckup = document.querySelector("#neckup");
+const shouldersup = document.querySelector("#shouldersup");
+const handsup = document.querySelector("#handsup");
 
-
-document.querySelector('#eyesnext').addEventListener('click', eyesnext)
+document.querySelector("#eyesnext").addEventListener("click", eyesnext);
 //document.querySelector('#necknext').addEventListener('click', necknext)
 //document.querySelector('#shouldersnext').addEventListener('click', shouldersnext)
 //document.querySelector('#handsnext').addEventListener('click', handsnext)
 
+function eyesnext() {
+  let filePath = "img/eyes/";
+  let eyeImages = [
+    "adream.png",
+    "background1.png",
+    "blurfloat.gif",
+    "couchlife.png",
+    "driftfloat.gif",
+  ];
+  //run random function so it spits out random number
 
-function eyesnext(){
-    let filePath = 'img/eyes/'
-    let eyeImages = ['adream.png','background1.png','blurfloat.gif', 'couchlife.png','driftfloat.gif']
-//run random function so it spits out random number
-
-//set src for eyes up to whatever image in our array has the index of the random number that was spit out
-eyesup.src = filePath + eyeImages[getRandomInt(5)]
-//google how to set up src using javascript
-//add function so that it doesnt toggle
-    neckup.classList.add('hidden')
-    shouldersup.classList.add('hidden')
-    handsup.classList.add('hidden')
-    eyesup.classList.toggle('hidden')
+  //set src for eyes up to whatever image in our array has the index of the random number that was spit out
+  eyesup.src = filePath + eyeImages[getRandomInt(5)];
+  //google how to set up src using javascript
+  //add function so that it doesnt toggle
+  neckup.classList.add("hidden");
+  shouldersup.classList.add("hidden");
+  handsup.classList.add("hidden");
+  eyesup.classList.toggle("hidden");
 }
 
 /*
@@ -53,12 +57,9 @@ function handsnext(){
 
 //create a function that creates random pictures to be shown upon a click event
 
-
 //Create a collection of images for each click event (applies to each click event)
 // create an aray with 5 image strings in it
 //finds the file path and calls it
-
-
 
 //console.log(filePath + eyeImages[0])
 //create variable that holds image paths
@@ -70,14 +71,8 @@ function handsnext(){
 //want a randomized number spit out from 0-4
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
-
-
-
-
-
+  return Math.floor(Math.random() * max);
+}
 
 //eye click event (5 gifs) [how are you going to do this?]
 
@@ -85,14 +80,11 @@ function getRandomInt(max) {
 //whatever picture is already up, needs to go to hidden
 //select one photo out of 5 in eyeclick array
 
-
 //want one randomly selected
 //need function that randomizes a number 0-4 that corresponds with image in array
 //once number is selected in array
 //pull corresponding number image from array
 //toggle image selected from hidden to displayed in display box
-
-
 
 //shoulder click event (5 images)
 //one image randomly displays per each click
@@ -102,4 +94,3 @@ function getRandomInt(max) {
 
 //hands click event (5 images)
 //one image randomly displays per each click
-
