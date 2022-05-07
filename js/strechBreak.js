3413a86b66c656292ee5b7210e800f2570c5238d
Lauri Jalonen <ljalonen@seatosummit.com.au>
2022-05-07 17:08:04 +0800

    stuff into a central state, modular functions

--- js/strechBreak.js
+++ js/strechBreak.js
@@ -1,96 +1,76 @@
-const eyesup = document.querySelector("#eyesup");
-const neckup = document.querySelector("#neckup");
-const shouldersup = document.querySelector("#shouldersup");
-const handsup = document.querySelector("#handsup");
+const state = {
+  eyes: {
+    visible: false,
+    container: "#eyesup",
+    uiElement: "#eyesnext",
+    images: [
+      "eyes/adream.png",
+      "eyes/blurfloat.gif",
+      "eyePalming.gif",
+      "palm.gif",
+    ],
+  },
+  neck: {
+    visible: false,
+    container: "#neckup",
+    uiElement: "#necknext",
+    images: ["blue.png", "bluecity.png", "bluecloud.png", "cloudgif.jpeg"],
+  },
+  shoulders: {
+    visible: false,
+    container: "#shouldersup",
+    uiElement: "#shouldersnext",
+    images: ["mushroom.png", "candle.png"],
+  },
+  hands: {
+    visible: false,
+    container: "#handsup",
+    uiElement: "#handnext",
+    images: ["rainy.gif", "pinkswirl.png"],
+  },
+};
 
-document.querySelector("#eyesnext").addEventListener("click", eyesnext);
-//document.querySelector('#necknext').addEventListener('click', necknext)
-//document.querySelector('#shouldersnext').addEventListener('click', shouldersnext)
-//document.querySelector('#handsnext').addEventListener('click', handsnext)
-
-function eyesnext() {
-  let filePath = "img/eyes/";
-  let eyeImages = [
-    "adream.png",
-    "background1.png",
-    "blurfloat.gif",
-    "couchlife.png",
-    "driftfloat.gif",
-  ];
-  //run random function so it spits out random number
-
-  //set src for eyes up to whatever image in our array has the index of the random number that was spit out
-  eyesup.src = filePath + eyeImages[getRandomInt(5)];
-  //google how to set up src using javascript
-  //add function so that it doesnt toggle
-  neckup.classList.add("hidden");
-  shouldersup.classList.add("hidden");
-  handsup.classList.add("hidden");
-  eyesup.classList.toggle("hidden");
+function init() {
+  for (const key in state) {
+    try {
+      const currentStateItem = state[key];
+      const uiElementDOMNode = document.querySelector(
+        currentStateItem.uiElement
+      );
+      currentStateItem.clickListener = uiElementDOMNode.addEventListener(
+        "click",
+        () => {
+          selectExercise({ key, item: currentStateItem });
+        }
+      );
+    } catch (error) {
+      console.error(
+        `Error intialising element ${key}. Check your HTML has the right identifiers.`
+      );
+    }
+  }
 }
 
-/*
-function necknext(){
-    eyesup.classList.add('hidden')
-    shouldersup.classList.add('hidden')
-    handsup.classList.add('hidden')
-    neckup.classList.toggle('hidden')
+function hideEverythingExcept(args) {
+  const skipThis = args?.skipThis;
+  for (const key in state) {
+    if (key === skipThis) continue;
+    const currentStateItem = state[key];
+    const currentDOMNode = document.querySelector(currentStateItem.container);
+    currentDOMNode.classList.add("hidden");
+  }
 }
 
-
-function shouldersnext(){
-    eyesup.classList.add('hidden')
-    handsup.classList.add('hidden')
-    neckup.classList.add('hidden')
-    shouldersup.classList.toggle('hidden')
+function selectExercise(args) {
+  const key = args?.key;
+  const item = args?.item;
+  hideEverythingExcept({ skipThis: key });
+  if (item) {
+    const randomPick = Math.floor(Math.random() * item.images.length);
+    const currentDOMNode = document.querySelector(item.container);
+    currentDOMNode.src = `img/${item.images[randomPick]}`;
+    currentDOMNode.classList.toggle("hidden");
+  }
 }
 
-
-function handsnext(){
-    eyesup.classList.add('hidden')
-    shouldersup.classList.add('hidden')
-    neckup.classList.add('hidden')
-    handsup.classList.toggle('hidden')
-}
-
-*/
-
-//create a function that creates random pictures to be shown upon a click event
-
-//Create a collection of images for each click event (applies to each click event)
-// create an aray with 5 image strings in it
-//finds the file path and calls it
-
-//console.log(filePath + eyeImages[0])
-//create variable that holds image paths
-
-//where are you going to store images? [in a folder, in its own folder? what would  make this easiest?]
-//pull images from where they are located and put them in an array
-
-//how do you want it selected randomly
-//want a randomized number spit out from 0-4
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-//eye click event (5 gifs) [how are you going to do this?]
-
-//one image randomly displays per each click
-//whatever picture is already up, needs to go to hidden
-//select one photo out of 5 in eyeclick array
-
-//want one randomly selected
-//need function that randomizes a number 0-4 that corresponds with image in array
-//once number is selected in array
-//pull corresponding number image from array
-//toggle image selected from hidden to displayed in display box
-
-//shoulder click event (5 images)
-//one image randomly displays per each click
-
-// neck click event (5 images)
-//one image randomly displays per each click
-
-//hands click event (5 images)
-//one image randomly displays per each click
+init();
